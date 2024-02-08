5eddbd2a2442263f55c755edc4cb6147ad5536e4
QuiteLifeM <atiyus12345@gmail.com>
2024-02-08 21:40:02 +0300

    2

--- Assets/DoText.cs
+++ Assets/DoText.cs
@@ -1,24 +1,20 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class DoText : MonoBehaviour
 {
-    [SerializeField] private Text _text1;
-    [SerializeField] private Text _text2;
-    [SerializeField] private Text _text3;
+    [SerializeField] private Text _textToReplace;
+    [SerializeField] private Text _textToAdd;
+    [SerializeField] private Text _textToScramble;
     [SerializeField] private float _duration;
 
     private void Start()    
     {
-        _text1.DOText("Это было не просто смело. Это было пиздец как смело.", _duration);
-        _text2.DOText(" Сомнительно, но Окэээй.", _duration).SetRelative();
-        _text3.DOText("Ну это пиздец какой-то просто, ну сколько можно.", _duration, true, ScrambleMode.All);
-    }
-
-    private void Awake()
-    {
-        _text1
+        _textToReplace.DOText("Это было не просто смело. Это было пиздец как смело.", _duration);
+        _textToAdd.DOText(" Сомнительно, но Окэээй.", _duration).SetRelative();
+        _textToScramble.DOText("Ну это пиздец какой-то просто, ну сколько можно.", _duration, true, ScrambleMode.All);
     }
 }
